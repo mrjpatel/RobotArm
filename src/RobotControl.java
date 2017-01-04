4f62f0b8e2c0427e440825d877f6700a024ad6ce
Japan Patel <japanpatel1198@gmail.com>
2017-01-04 21:28:29 +1100

    Adding project minus the algorithm (initial commit)

---
+++ src/RobotControl.java
@@ -0,0 +1,11 @@
+class RobotControl {
+	private Robot r;
+
+	public RobotControl(Robot r) {
+		this.r = r;
+	}
+
+	public void control(int barHeights[], int blockHeights[]) {
+
+	} //end of method
+} //end of class
